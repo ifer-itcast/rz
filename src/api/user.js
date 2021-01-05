cf46091d0c57d0227baef932f4625ae802e0bd6d
ifer-itcast <yangkangkang@itcast.cn>
2021-01-05 10:19:52 +0800

    封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,15 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
+/**
+ * 登录
+ * @param {*} data
+ */
 export function login(data) {
-
+  return request({
+    url: '/sys/login',
+    method: 'POST',
+    data
+  })
 }
 
 export function getInfo(token) {
