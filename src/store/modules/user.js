9bac4bd241ae3f061f1f445c136fabcfe35829b9
ifer-itcast <yangkangkang@itcast.cn>
2021-01-05 12:23:01 +0800

    Vuex中操作token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,21 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+const state = {
+  token: getToken()
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
