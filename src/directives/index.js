19f905f0fdfa3903943d3646fdb7e697f90754c2
ifer-itcast <yangkangkang@itcast.cn>
2021-01-06 22:49:12 +0800

    自定义imgerror指令

---
+++ src/directives/index.js
@@ -0,0 +1,10 @@
+export const imgerror = {
+  inserted(dom, options) {
+    // 监听 img 标签的错误事件 onerror
+    dom.src = dom.src || options.src
+    dom.onerror = function() {
+      // 给一个默认的图片
+      dom.src = options.value
+    }
+  }
+}
