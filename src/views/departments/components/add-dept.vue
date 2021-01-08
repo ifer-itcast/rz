5fb6f18d4a1b28ac04203798c0ff02c56e687bb6
ifer-itcast <yangkangkang@itcast.cn>
2021-01-08 17:17:48 +0800

    获取部门负责人数据并渲染

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -10,7 +10,9 @@
         <el-input v-model="formData.code" style="width:80%" placeholder="1-50个字符" />
       </el-form-item>
       <el-form-item label="部门负责人" prop="manager">
-        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择" />
+        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择" @focus="getEmployeeSimple">
+          <el-option v-for="item in peoples" :key="item.id" :lable="item.username" :value="item.username" />
+        </el-select>
       </el-form-item>
       <el-form-item label="部门介绍" prop="introduce">
         <el-input v-model="formData.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
@@ -27,6 +29,7 @@
 </template>
 <script>
 import { getDepartments } from '@/api/departments'
+import { getEmployeeSimple } from '@/api/employees'
 export default {
   props: {
     showDialog: {
@@ -80,7 +83,13 @@ export default {
           { required: true, message: '部门介绍不能为空', trigger: 'blur' },
           { trigger: 'blur', min: 1, max: 300, message: '部门介绍要求1-50个字符' }
         ]
-      }
+      },
+      peoples: [] // 员工简单信息列表
+    }
+  },
+  methods: {
+    async  getEmployeeSimple() {
+      this.peoples = await getEmployeeSimple()
     }
   }
 }
