8f38bd21fb52af2f21125c65ff6511c7c9f0fbac
ifer-itcast <yangkangkang@itcast.cn>
2021-01-08 11:43:25 +0800

    .

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,6 +2,26 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card class="tree-card">
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px;">
+          <el-col>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作<i class="el-icon-arrow-down el-icon--right" />
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
           <el-row slot-scope="{data}" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%;">
             <el-col>
