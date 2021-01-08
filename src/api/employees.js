4fe389bf6f496004356869044261e729ddd5dcad
ifer-itcast <yangkangkang@itcast.cn>
2021-01-08 23:01:06 +0800

    获取员工列表数据并渲染

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,10 +1,21 @@
 import request from '@/utils/request'
 
 /**
- * 获取员工的简单列表
+ * 获取员工的简单信息列表
  */
 export function getEmployeeSimple() {
   return request({
     url: '/sys/user/simple'
+  })
+}
+
+/**
+ * 获取员工的综合信息列表
+ * @param {object} params
+ */
+export function getEmployeeList(params) {
+  return request({
+    url: '/sys/user',
+    params
   })
 }
