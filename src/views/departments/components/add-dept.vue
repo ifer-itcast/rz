956087e25e7da8837ef7dc989877bc88df209e82
ifer-itcast <yangkangkang@itcast.cn>
2021-01-08 16:34:25 +0800

    配置新增表单的基本校验

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -42,6 +42,21 @@ export default {
         introduce: ''
       },
       rules: {
+        name: [
+          { required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门名称要求1-50个字符', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门编码要求1-50个字符', trigger: 'blur' }
+        ],
+        manager: [
+          { required: true, message: '部门负责人不能为空', trigger: 'blur' }
+        ],
+        introduce: [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          { trigger: 'blur', min: 1, max: 300, message: '部门介绍要求1-50个字符' }
+        ]
       }
     }
   }
