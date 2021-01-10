851326840a190848b0fd5c03c1b8561c5a102ba6
ifer-itcast <yangkangkang@itcast.cn>
2021-01-10 23:31:52 +0800

    导入的时间问题

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -20,12 +20,29 @@ export default {
       results.forEach(item => {
         const userInfo = {}
         Object.keys(item).forEach(key => {
+          // key 是当前的中文名，找到对应的英文名
+          if (userRelations[key] === 'timeOfEntry' || userRelations[key] === 'correctionTime') {
+            userInfo[userRelations[key]] = new Date(this.formatDate(item[key], '/'))
+            return
+          }
           userInfo[userRelations[key]] = item[key]
         })
         arr.push(userInfo)
       })
-      await importEmployee(arr) // 调用导入接口
+      await importEmployee(arr)
+      this.$message.success('导入成功')
       this.$router.back()
+    },
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
     }
   }
 }
