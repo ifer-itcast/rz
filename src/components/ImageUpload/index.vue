c52869e86c6c8cec371b7e85cba4ab915ff6b2a0
ifer-itcast <yangkangkang@itcast.cn>
2021-01-12 11:32:13 +0800

    点击图片进行预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <el-upload list-type="picture-card">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload list-type="picture-card" :limit="1" action="#" :on-preview="preview" :file-list="fileList">
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog title="图片" :visible.sync="showDialog">
+      <img :src="imgUrl" style="width:100%" alt="">
+    </el-dialog>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      fileList: [{
+        url: 'http://file01.16sucai.com/d/file/2011/0801/20110801111724537.jpg'
+      }], // 图片地址设置为数组
+      showDialog: false, // 控制显示弹层
+      imgUrl: ''
+    }
+  },
+  methods: {
+    preview(file) {
+      this.imgUrl = file.url
+      this.showDialog = true
+    }
+  }
+}
+</script>
