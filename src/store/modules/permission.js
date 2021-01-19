fd1a588f8abd2a728147baa1e8285a245b34e871
ifer-itcast <yangkangkang@itcast.cn>
2021-01-19 23:16:35 +0800

    筛选权限路由

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,4 +1,4 @@
-import { constantRoutes } from '@/router' // 静态路由表
+import { constantRoutes, asyncRoutes } from '@/router' // 静态路由表
 const state = {
   routes: constantRoutes
 }
@@ -8,7 +8,19 @@ const mutations = {
     state.routes = [...constantRoutes, ...newRoutes]
   }
 }
-const actions = {}
+
+const actions = {
+  // 筛选出动态路由中能够和 menus 能够对应上的路由
+  filterRoutes(context, menus) {
+    const routes = []
+    menus.forEach(key => {
+      routes.push(...asyncRoutes.filter(item => item.name === key))
+    })
+    context.commit('setRoutes', routes)
+    return routes
+  }
+}
+
 export default {
   namespaced: true,
   state,
