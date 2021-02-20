3ed759240c1b73a777762d8562a1f633d3402747
ifer-itcast <yangkangkang@itcast.cn>
2021-02-20 22:18:00 +0800

    关于nprogress.done在说明

--- src/permission.js
+++ src/permission.js
@@ -11,10 +11,13 @@ const whiteList = ['/login', '/404']
 router.beforeEach((to, from, next) => {
   // 开启进度条
   nprogress.start()
+  // !#1
   if (store.getters.token) {
     // 有 token
     if (to.path === '/login') {
+      // !#2
       // 如果要访问的是登录页，跳转到主页
+      // !#3
       next('/')
     } else {
       next()
@@ -29,7 +32,12 @@ router.beforeEach((to, from, next) => {
       next('/login')
     }
   }
-  // 为了解决手动切换地址时进度条的不关闭的问题，例如 /dashboard 再手动输入 /login
+  // 为了解决切换地址时进度条的不关闭的问题，例如在 /dashboard 时再输入 /login
+  // 从 /dashboard 到 /login（注意此时还没跳转完成），因为是不同的地址会触发 beforeEach
+  // 先走 #1
+  // 然后 #2，跳转到 /，然后再重定向到 /dashboard
+  // 此时变成了同一个地址间的跳转，并不会再次触发 beforeEach 和 afterEach
+  // 可以尝试不同地址跳转的现象，例如把 #3 处的代码随意改成 next('/xxx')，则会再次触发 beforeEach 和 afterEach
   nprogress.done()
 })
 
