041d4fa159541b935162cbca30234413b825023d
ifer-itcast <yangkangkang@itcast.cn>
2021-03-01 11:07:36 +0800

    改变路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -66,7 +66,8 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
+  base: 'hr/',
   scrollBehavior: () => ({ y: 0 }),
   routes: [...constantRoutes] // 合并静态路由和动态路由
 })
