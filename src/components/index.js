749d006807797a0bab86c1fcc93605922b124643
ifer-itcast <yangkangkang@itcast.cn>
2021-03-01 09:43:51 +0800

    全屏功能

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
 import Print from 'vue-print-nb'
+import ScreenFull from './ScreenFull'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
     Vue.component('ImageUpload', ImageUpload)
     Vue.use(Print)
+    Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
   }
 }
